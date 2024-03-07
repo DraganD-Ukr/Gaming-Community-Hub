b847fb6716e15d06b57c63f3f0a7ab0968030060
DraganD-Ukr <dmitrijrolkin@gmail.com>
2024-03-07 11:47:22 +0000

    Styled the home page to fit dark mode

--- gaming_community_hub/app/page.tsx
+++ gaming_community_hub/app/page.tsx
@@ -1,20 +1,15 @@
 'use client'
 
-import { useState } from 'react'
-import Navbar from '@/components/Navbar'
-
-
-
-
+import { useState } from 'react';
+import Navbar from '@/components/Navbar';
 
 export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="bg-white">
-      
+    <div className="bg-gray-900"> {/* Change bg-white to bg-gray-900 */}
       {/* Navbar */}
-      <Navbar/>
+      <Navbar />
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
@@ -31,7 +26,7 @@ export default function Example() {
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-300 ring-1 ring-gray-200/30 hover:ring-gray-500/20 duration-300 ease-in-out">
               Announcing our next round of funding.{' '}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
@@ -40,10 +35,10 @@ export default function Example() {
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
               Data to enrich your online business
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="mt-6 text-lg leading-8 text-gray-300">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
               fugiat veniam occaecat fugiat aliqua.
             </p>
@@ -54,7 +49,7 @@ export default function Example() {
               >
                 Get started
               </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              <a href="#" className="text-sm font-semibold leading-6 text-white dark:hover:bg-gray-700 dark:hover:bg-opacity-30 rounded-full px-4 py-2 transition-colors duration-300 ease-in-out ">
                 Learn more <span aria-hidden="true">→</span>
               </a>
             </div>
