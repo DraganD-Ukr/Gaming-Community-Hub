6e93e63ea2f753dfe2c76ceb694402006f628b22
Dmytro Drahan <dmitrijrolkin@gmail.com>
2024-03-14 21:46:48 +0000

    Speed Insights for Vercel

--- gaming_community_hub/app/layout.tsx
+++ gaming_community_hub/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar/>
         {children}
+        <SpeedInsights/>
         <Footer/>
         </body>
     </html>
